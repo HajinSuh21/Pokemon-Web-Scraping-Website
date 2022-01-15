c71c61b8387d946f658c5a0a180c2ee5851ecc4d
HajinSuh21 <97658524+HajinSuh21@users.noreply.github.com>
2022-01-15 00:39:15 -1000

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -2,11 +2,9 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'key'
 
-    from .scraping import views, sets
+    from .scraping import sets
 
-    app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(sets, url_prefix='/')
 
     return app
